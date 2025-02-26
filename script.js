2319bdc6ea58d79db2538b18b47b78828b9af907
DevotedMiniPhase <134955350+DevotedMiniPhase@users.noreply.github.com>
2025-02-26 16:31:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'api_key';
+const API_KEY = 'AIzaSyCH_RQKrQhJX7OtKuZ4ysfbsuzT87sDF6Q';
 
 async function fetchTalks() {
     const searchType = document.getElementById("searchType").value;
